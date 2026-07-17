40377b8809710daa206301c439bb2b39384f9e20
Weijie Chen <57164526+TheLegend99@users.noreply.github.com>
2026-07-17 00:02:06 -0400

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        const stack = [];
+        let result = 0;
+        for (let i = 0; i < height.length; i ++){
+            while (stack.length > 0 && height[stack[stack.length-1]] < height[i]){
+                const midIdx = stack.pop();
+                if (stack.length > 0){
+                    const leftIdx = stack[stack.length - 1];
+                    result += (Math.min(height[leftIdx], height[i]) - height[midIdx]) * (i - leftIdx - 1);
+                }
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
