ad98488bfef43444be61dab3289997b7908d78c3
Weijie Chen <57164526+TheLegend99@users.noreply.github.com>
2026-08-11 17:09:48 -0400

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(nums) {
+        const result = [];
+        const path = [];
+        nums.sort((a, b) => a - b);
+        const backTracking = function(start){
+            if (start > nums.length) return;
+            result.push([...path]);
+            for (let i = start; i < nums.length; i++){
+                if (i > start && nums[i - 1] === nums[i]) continue;
+                path.push(nums[i]);
+                backTracking(i+1);
+                path.pop();
+            }
+        }
+        backTracking(0);
+        return result;
+    }
+}
