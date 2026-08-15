d0108527e4db8247b14aeaa3489d5ba906a8f8bf
Weijie Chen <57164526+TheLegend99@users.noreply.github.com>
2026-08-15 16:18:20 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.js
@@ -0,0 +1,49 @@
+class WordDictionary {
+    trie
+    constructor() {
+        this.trie = {};
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        const helper = function(idx, node){
+            if (idx === word.length){
+                node["word"] = true;
+                return;
+            }
+            if (!(word[idx] in node)){
+                node[word[idx]] = {};
+            }
+            helper(idx + 1, node[word[idx]]);
+        }
+        helper(0, this.trie);
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        const helper = function(idx, node){
+            if (idx === word.length && node["word"]){
+                return true;
+            }
+            if (word[idx] !== "."){
+                if (!(word[idx] in node)) return false;
+                return helper(idx+1, node[word[idx]]);
+            } else {
+                for (let key in node){
+                    if (key === "word") continue;
+                    if (helper(idx+1, node[key])){
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return helper(0, this.trie);
+    }
+}
