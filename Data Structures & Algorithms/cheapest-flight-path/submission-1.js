3442f8f5936ef1353129616e8a79155cd4add53e
Weijie Chen <57164526+TheLegend99@users.noreply.github.com>
2026-08-22 17:06:04 -0400

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} flights
+     * @param {number} src
+     * @param {number} dst
+     * @param {number} k
+     * @return {number}
+     */
+    findCheapestPrice(n, flights, src, dst, k) {
+        
+        let minDist = new Array(n).fill(Infinity);
+        minDist[src] = 0;
+        for (let i = 0; i < k + 1 ; i++){
+            const preMinDist = [...minDist];
+            for (const [u, v, w] of flights){
+                if (minDist[u] !== Infinity && preMinDist[u] + w < minDist[v]){
+                    minDist[v] = preMinDist[u] +  w;
+                }
+            }
+           
+        }
+        return minDist[dst] === Infinity ? -1 : minDist[dst];
+    }
+}
