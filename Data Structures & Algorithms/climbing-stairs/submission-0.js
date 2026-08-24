162c8e690bdde019dba4acf586de1a01d66a4010
Weijie Chen <57164526+TheLegend99@users.noreply.github.com>
2026-08-24 17:48:15 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const dp = new Array(n+1).fill(0);
+        dp[1] = 1;
+        dp[0] = 1;
+        for (let i = 2; i <= n; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n];
+
+    }
+}
